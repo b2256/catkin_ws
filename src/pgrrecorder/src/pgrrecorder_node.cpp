b87dda2c6284f8daffcdd9ae6672fd60a40eb72e
ghedger <greg@hedgersoftware.com>
2019-07-12 18:23:01 -0600

    Added color to monitor image; added asymetric panning capability, with a "fixate" lens for a separate duration from the other lenses.

--- src/pgrrecorder/src/pgrrecorder_node.cpp
+++ src/pgrrecorder/src/pgrrecorder_node.cpp
@@ -12,6 +12,7 @@
 
 // ROS stuff
 #include <ros/ros.h>
+#include <ros/time.h>
 #include <sensor_msgs/image_encodings.h>
 #include <sensor_msgs/Image.h>
 #include <sensor_msgs/CameraInfo.h>
@@ -55,6 +56,13 @@ static volatile int running_ = 1;
 image_transport::ImageTransport *it_;
 image_transport::Publisher image_pub_;
 
+static int monitor_lens_ = 0;
+static int current_pan_lens_ = 0;
+static double monitor_fixate_duration_ = 0;
+static double monitor_pan_duration_ = 0;
+static ros::Time last_lens_update_;
+static ros::Time next_lens_update_;
+
 // InitPublishMonitor
 // Init publication of the camera-is-running monitor image
 void InitPublishMonitor(ros::NodeHandle nh)
@@ -76,14 +84,16 @@ void DeInitPublishMonitor()
 // getImageIndex
 // Find image nth index (according to JPG headers) past the index passed in in a stream of JPEG d in a string of data.
 // Looks for jpeg header data and, if found, looks around it to confirm it.
-char *getImageIndex(int start_index, int seek_index, int max_size, char *data)
+char *getImageIndex(int seek_image_index, int max_size, char *data)
 {
   unsigned char *image_location = (unsigned char *) data;
   unsigned char *image_candidate_location;
   unsigned char *end = data + max_size;
-  char index = start_index;       // running count index
-
   int current_image_index = 0;
+
+  if (!seek_image_index) {
+    return image_location;
+  }
 
   // Table contains candidate index deltas to beginning of string
   static char delta_lookup[256] =
@@ -101,13 +111,13 @@ char *getImageIndex(int start_index, int seek_index, int max_size, char *data)
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       // 0xa0
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       // 0xb0
     0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       // 0xc0
-    0, 0, 0, 0, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0, 0, 0,       // 0xd0
-    -3,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       // 0xe0
-    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -2,       // 0xf0-0xff
+    0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,       // 0xd0
+    3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,       // 0xe0
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2,       // 0xf0-0xff
   };
 
   // Don't start on the first JPEG header; advance index
-  image_location += 4;
+  //image_location += 4;
 
   // Since JPEG doesn't have compressed image size (see https://www.file-recovery.com/jpg-signature-format.htm)
   // we are faced with the arduous task of searching for the next JPEG header
@@ -116,17 +126,36 @@ char *getImageIndex(int start_index, int seek_index, int max_size, char *data)
     int delta_candidate_index;
     // Yes, assignment below of delta_candidate_index is deliberate
     if (delta_candidate_index = delta_lookup[*image_location]) {
-      image_candidate_location = image_location + delta_candidate_index;
-      if (0xff == image_candidate_location[0])
-        if (0xd8 == image_candidate_location[0])
-          if (0xff == image_candidate_location[0])
-            if (0xe0 == image_candidate_location[0]) {
+      image_candidate_location = image_location - delta_candidate_index;
+      if ((unsigned char)0xff == image_candidate_location[0]) {
+        if ((unsigned char)0xd8 == image_candidate_location[1]) {
+          if ((unsigned char)0xff == image_candidate_location[2]) {
+            if ((unsigned char)0xe0 == image_candidate_location[3]) {
               current_image_index++;
-              if (current_image_index == seek_index) {
+	            //cout << "Index incremented" << endl;
+              if (current_image_index == seek_image_index) {
                 image_location = image_candidate_location;
                 break;
               }
             }
+          }
+        }
+      }
+    }
+    image_candidate_location = image_location;
+    if ((unsigned char)0xff == image_candidate_location[0]) {
+      if ((unsigned char)0xd8 == image_candidate_location[1]) {
+        if ((unsigned char)0xff == image_candidate_location[2]) {
+          if ((unsigned char)0xe0 == image_candidate_location[3]) {
+            current_image_index++;
+	          //cout << "Index incremented" << endl;
+            if (current_image_index == seek_image_index) {
+              image_location = image_candidate_location;
+              break;
+            }
+          }
+        }
+      }
     }
   }
 
@@ -137,21 +166,44 @@ char *getImageIndex(int start_index, int seek_index, int max_size, char *data)
 // Peridocially publish one color channel from a single camera lens for
 // camera operation monitoring purposes.
 // Entry: LadybugImage
-void PublishMonitorImage(ros::NodeHandle nh, LadybugImage& currentImage)
+void PublishMonitorImage(ros::NodeHandle nh, LadybugImage& currentImage, int lens_index)
 {
 	// Publish image from the first camera.  Note that the offset of this
 	// image is known.  It is possible to publish images from other cameras, but
 	// their starting locations are unknown as each JPEG image of a given size
 	// will fluctuate owing to the compression.
 #define LADYBUG3_IMOFFS 1024
-	char *buffer;
-  buffer = getImageIndex(0, 3, currentImage.uiDataSizeBytes, (char *)&currentImage.pData[LADYBUG3_IMOFFS]);
+	char *buffer, *bufferR, *bufferG, *bufferB;
+  bufferR = getImageIndex((lens_index * 4), 
+    currentImage.uiDataSizeBytes - LADYBUG3_IMOFFS, 
+    (char *)&currentImage.pData[LADYBUG3_IMOFFS]
+  );
+  bufferG = getImageIndex(1, 
+    currentImage.uiDataSizeBytes - LADYBUG3_IMOFFS, 
+    bufferR
+  );
+  bufferB = getImageIndex(1,
+    currentImage.uiDataSizeBytes - LADYBUG3_IMOFFS, 
+    bufferG
+  );
 
-  buffer = (char *)&currentImage.pData[LADYBUG3_IMOFFS];
+  //buffer = (char *)&currentImage.pData[LADYBUG3_IMOFFS];
 	// cv::imdecode will detect JPEG format from the header bytes (0xff 0xf8 0xff 0xe0)
 	// It will be decompressed into a 1-dimensional matrix (matImg) with its
 	// own buffer, which we then publish.
-	cv::Mat matImg = cv::imdecode(cv::Mat(1, currentImage.uiDataSizeBytes, CV_8UC1, buffer), cv::IMREAD_UNCHANGED);
+	//cv::Mat matImg = cv::imdecode(cv::Mat(1, currentImage.uiDataSizeBytes, CV_8UC1, buffer), cv::IMREAD_UNCHANGED);
+	cv::Mat r = cv::imdecode(cv::Mat(1, currentImage.uiDataSizeBytes, CV_8UC1, bufferR), cv::IMREAD_UNCHANGED);
+	cv::Mat g = cv::imdecode(cv::Mat(1, currentImage.uiDataSizeBytes, CV_8UC1, bufferG), cv::IMREAD_UNCHANGED);
+	cv::Mat b = cv::imdecode(cv::Mat(1, currentImage.uiDataSizeBytes, CV_8UC1, bufferB), cv::IMREAD_UNCHANGED);
+
+  std::vector<cv::Mat> array_to_merge;
+  array_to_merge.push_back(b);
+  array_to_merge.push_back(g);
+  array_to_merge.push_back(r);
+  cv::Mat color;
+  cv::merge(array_to_merge, color);
+
+  transpose(color, color);
 
   #if 0
   // Comment in if the image must be rotated in the frame by 90 degrees.
@@ -161,11 +213,37 @@ void PublishMonitorImage(ros::NodeHandle nh, LadybugImage& currentImage)
   cv::Rect2f bbox = cv::RotatedRect(cv::Point2f(), matImg.size(), angle)
   #endif
 
-  transpose(matImg, matImg);
+  //transpose(matImg, matImg);
 
-	sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", matImg).toImageMsg();		// image for publication
+	sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", color).toImageMsg();		// image for publication
+	//sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", matImg).toImageMsg();		// image for publication
 	msg->header.stamp = ros::Time::now();
 	image_pub_.publish(msg);
+}
+
+// UpdateLens
+// Update the lens that I use
+// Entry: -
+// Exit: -
+void UpdateLens()
+{
+  double duration;
+  if (current_pan_lens_ == monitor_lens_) {
+    duration = monitor_fixate_duration_;
+  } else {
+    duration = monitor_pan_duration_;
+  }
+
+  cout << duration << " " << next_lens_update_.toSec() << " " << last_lens_update_.toSec() << endl;
+  if (last_lens_update_.toSec() > next_lens_update_.toSec()) {
+    last_lens_update_ = next_lens_update_;
+    next_lens_update_ += ros::Duration(duration);
+    current_pan_lens_++;
+    current_pan_lens_ %= 6;      // TODO: Hardcoded lens max; look up in Ladybug SDK!!! 
+    cout << "2" << endl;
+  } else {
+    last_lens_update_ = ros::Time::now();
+  }
 }
 
 // GrabLoop
@@ -190,7 +268,7 @@ void GrabLoop( ros::NodeHandle nh, ImageGrabber &grabber, ImageRecorder &recorde
   {
     static int fidx = 0;
     FILE *pf;
-    char *fpath = "/home/afl03/catkin_ws/notes/cameraraw";
+    char *fpath = "/home/afl04/catkin_ws/notes/cameraraw";
     char buf[256];
     sprintf(buf, "%s%d", fpath, fidx);
     fidx++;
@@ -207,7 +285,7 @@ void GrabLoop( ros::NodeHandle nh, ImageGrabber &grabber, ImageRecorder &recorde
 		static int monitor_count = 0;
 		// Only publish every few frames -- a couple times a second should suffice.
 		//if(!(++monitor_count & 0x07))	// TODO: parameterize this
-			PublishMonitorImage(nh, currentImage);
+			PublishMonitorImage(nh, currentImage, current_pan_lens_);
 
 		double mbWritten = 0.0;
 		unsigned long imagesWritten = 0;
@@ -222,6 +300,10 @@ void GrabLoop( ros::NodeHandle nh, ImageGrabber &grabber, ImageRecorder &recorde
 		// cout << imagesWritten << " images - " << mbWritten << "MB" << endl;
 
 		grabber.Unlock(currentImage.uiBufferIndex);
+
+    // Swap lens
+    UpdateLens();
+
 		ros::spinOnce();
 	}
 }
@@ -243,7 +325,7 @@ bool setCameraParams(ConfigurationProperties& config)
 {
   double param;
   bool success = true;
-  if (ros::param::has("gain") && ros::param::get("gain", param)) {
+  if (ros::param::has("~gain") && ros::param::get("~gain", param)) {
     if (param) {
       if (param >= kGainMin && param < kGainMax) {
         config.camera.gain = param;
@@ -255,7 +337,7 @@ bool setCameraParams(ConfigurationProperties& config)
       }
     }
   }
-  if (ros::param::has("brightness") && ros::param::get("brightness", param)) {
+  if (ros::param::has("~brightness") && ros::param::get("~brightness", param)) {
     if (param) {
       if (param >= kBrightnessMin && param < kBrightnessMax) {
         config.camera.brightness = param;
@@ -267,7 +349,7 @@ bool setCameraParams(ConfigurationProperties& config)
       }
     }
   }
-  if (ros::param::has("autoexposure") && ros::param::get("autoexposure", param)) {
+  if (ros::param::has("~autoexposure") && ros::param::get("~autoexposure", param)) {
     if (param) {
       if (param >= kAutoExposureMin && param < kAutoExposureMax) {
         config.camera.autoexposure = param;
@@ -279,7 +361,7 @@ bool setCameraParams(ConfigurationProperties& config)
       }
     }
   }
-  if (ros::param::has("whitebalance") && ros::param::get("whitebalance", param)) {
+  if (ros::param::has("~whitebalance") && ros::param::get("~whitebalance", param)) {
     if (param) {
       if (param >= kWhiteBalanceMin && param < kWhiteBalanceMax) {
         config.camera.whitebalance = param;
@@ -303,7 +385,7 @@ bool setCameraParams(ConfigurationProperties& config)
       }
     }
   }
-  if (ros::param::has("shutter") && ros::param::get("shutter", param)) {
+  if (ros::param::has("~shutter") && ros::param::get("~shutter", param)) {
     if (param) {
       if (param >= kShutterMin && param < kShutterMax) {
         config.camera.shutter = param;
@@ -315,6 +397,37 @@ bool setCameraParams(ConfigurationProperties& config)
       }
     }
   }
+
+  int iparam;
+  if (ros::param::has("~monitor_lens") && ros::param::get("~monitor_lens", iparam)) {
+    if (iparam) {
+      if (iparam >= 0 && iparam < 6) {    // TODO: Make constants (lens min and max)
+        monitor_lens_ = current_pan_lens_ = iparam;
+        ROS_INFO("Set monitor lens to %d", iparam);
+      } else {
+        ROS_WARN("Monitor lens must be between 0 and 5.");
+        success = false;
+      }
+    }
+  }
+
+  if (ros::param::has("~monitor_fixate") && ros::param::get("~monitor_fixate", param)) {
+    if (param) {
+        monitor_fixate_duration_ = param;
+        ROS_INFO("Set monitor fixate to %f seconds", param);
+    }
+  }
+
+  if (ros::param::has("~monitor_pan") && ros::param::get("~monitor_pan", param)) {
+    if (param) {
+        monitor_pan_duration_ = param;
+        ROS_INFO("Set monitor pan to %f seconds", param);
+    }
+  }
+
+  last_lens_update_ = ros::Time::now();
+  next_lens_update_ = ros::Time::now() + ros::Duration(monitor_fixate_duration_); // = ros::Time::now();
+
   // END GPH
   return success;
 }
